7dfe5b355ac691ef2f6dd1d9ad9d01b85afdd690
denis08090 <161850677+denis08090@users.noreply.github.com>
2025-01-27 19:49:31 +0100

    adjust contollers and DTOs

---
+++ target/generated-sources/annotations/com/example/Bildergalerie/model/user/dto/UserMapperImpl.java
@@ -0,0 +1,127 @@
+package com.example.Bildergalerie.model.user.dto;
+
+import com.example.Bildergalerie.model.user.User;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-01-27T19:06:58+0100",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 22.0.2 (Oracle Corporation)"
+)
+@Component
+public class UserMapperImpl implements UserMapper {
+
+    @Override
+    public User fromDTO(UserDTO dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        User user = new User();
+
+        user.setId( dto.getId() );
+        user.setUserName( dto.getUserName() );
+        user.setFirstName( dto.getFirstName() );
+        user.setLastName( dto.getLastName() );
+        user.setEmail( dto.getEmail() );
+        user.setPassword( dto.getPassword() );
+
+        return user;
+    }
+
+    @Override
+    public List<User> fromDTOs(List<UserDTO> dtos) {
+        if ( dtos == null ) {
+            return null;
+        }
+
+        List<User> list = new ArrayList<User>( dtos.size() );
+        for ( UserDTO userDTO : dtos ) {
+            list.add( fromDTO( userDTO ) );
+        }
+
+        return list;
+    }
+
+    @Override
+    public Set<User> fromDTOs(Set<UserDTO> dtos) {
+        if ( dtos == null ) {
+            return null;
+        }
+
+        Set<User> set = new LinkedHashSet<User>( Math.max( (int) ( dtos.size() / .75f ) + 1, 16 ) );
+        for ( UserDTO userDTO : dtos ) {
+            set.add( fromDTO( userDTO ) );
+        }
+
+        return set;
+    }
+
+    @Override
+    public UserDTO toDTO(User BO) {
+        if ( BO == null ) {
+            return null;
+        }
+
+        UserDTO userDTO = new UserDTO();
+
+        userDTO.setId( BO.getId() );
+        userDTO.setUserName( BO.getUserName() );
+        userDTO.setFirstName( BO.getFirstName() );
+        userDTO.setLastName( BO.getLastName() );
+        userDTO.setEmail( BO.getEmail() );
+        userDTO.setPassword( BO.getPassword() );
+
+        return userDTO;
+    }
+
+    @Override
+    public List<UserDTO> toDTOs(List<User> BOs) {
+        if ( BOs == null ) {
+            return null;
+        }
+
+        List<UserDTO> list = new ArrayList<UserDTO>( BOs.size() );
+        for ( User user : BOs ) {
+            list.add( toDTO( user ) );
+        }
+
+        return list;
+    }
+
+    @Override
+    public Set<UserDTO> toDTOs(Set<User> BOs) {
+        if ( BOs == null ) {
+            return null;
+        }
+
+        Set<UserDTO> set = new LinkedHashSet<UserDTO>( Math.max( (int) ( BOs.size() / .75f ) + 1, 16 ) );
+        for ( User user : BOs ) {
+            set.add( toDTO( user ) );
+        }
+
+        return set;
+    }
+
+    @Override
+    public User fromUserRegisterDTO(UserRegisterDTO dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        User user = new User();
+
+        user.setId( dto.getId() );
+        user.setFirstName( dto.getFirstName() );
+        user.setLastName( dto.getLastName() );
+        user.setEmail( dto.getEmail() );
+        user.setPassword( dto.getPassword() );
+
+        return user;
+    }
+}
