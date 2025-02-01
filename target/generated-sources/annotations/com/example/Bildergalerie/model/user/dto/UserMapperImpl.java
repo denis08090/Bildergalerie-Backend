3abe5efcce5acad2e2fd4c873312b9764da744a2
chickenmicken <161852817+chickenmicken@users.noreply.github.com>
2025-02-01 11:38:10 +0100

    merge conflict

--- target/generated-sources/annotations/com/example/Bildergalerie/model/user/dto/UserMapperImpl.java
+++ target/generated-sources/annotations/com/example/Bildergalerie/model/user/dto/UserMapperImpl.java
@@ -10,8 +10,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-02-01T10:48:17+0100",
-    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 22.0.2 (Oracle Corporation)"
+    date = "2025-02-01T11:36:41+0100",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 22.0.2 (Amazon.com Inc.)"
 )
 @Component
 public class UserMapperImpl implements UserMapper {
